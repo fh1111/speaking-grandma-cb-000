f3eb470d496633ae2628a792920422f016d4262e
Fer Hernandez <fh.1111@gmail.com>
2018-10-28 16:09:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,12 +3,11 @@
 def speak_to_grandma(text)
 
   if ( !(text == text.upcase))
-
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif (text == "I LOVE YOU GRANDMA!")
-    puts ("I LOVE YOU TOO PUMPKIN!")
+    return "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 
 end
